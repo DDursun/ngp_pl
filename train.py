ebd52ead095365ab94054ccdd170e0cec2f310f9
Dursun Dashdamirov <dursun.dashdemirov.std@bhos.edu.az>
2024-09-15 18:45:38 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,293 +1,197 @@
-import torch
-from torch import nn
 from opt import get_opts
-import os
-import glob
-import imageio
-import numpy as np
-import cv2
-from einops import rearrange
+import torch
+from collections import defaultdict
 
-# data
 from torch.utils.data import DataLoader
 from datasets import dataset_dict
-from datasets.ray_utils import axisangle_to_R, get_rays
 
 # models
-from kornia.utils.grid import create_meshgrid3d
-from models.networks import NGP
-from models.rendering import render, MAX_SAMPLES
+from models.nerf import Embedding, NeRF
+from models.rendering import render_rays
 
-# optimizer, losses
-from apex.optimizers import FusedAdam
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from losses import NeRFLoss
+# optimizer, scheduler, visualization
+from utils import *
+
+# losses
+from losses import loss_dict
 
 # metrics
-from torchmetrics import (
-    PeakSignalNoiseRatio, 
-    StructuralSimilarityIndexMeasure
-)
-from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from metrics import *
 
 # pytorch-lightning
-from pytorch_lightning.plugins import DDPPlugin
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
-from pytorch_lightning.fabric.loggers import TensorBoardLogger
-from pytorch_lightning.utilities.distributed import all_gather_ddp_if_available
-
-from utils import slim_ckpt, load_ckpt
-
-import warnings; warnings.filterwarnings("ignore")
-
-
-def depth2img(depth):
-    depth = (depth-depth.min())/(depth.max()-depth.min())
-    depth_img = cv2.applyColorMap((depth*255).astype(np.uint8),
-                                  cv2.COLORMAP_TURBO)
-
-    return depth_img
-
+from pytorch_lightning.loggers import TestTubeLogger
+from pytorch_lightning.profiler import PyTorchProfiler
 
 class NeRFSystem(LightningModule):
     def __init__(self, hparams):
-        super().__init__()
+        super(NeRFSystem, self).__init__()
         self.save_hyperparameters(hparams)
+        # self.hparams = hparams
 
-        self.warmup_steps = 256
-        self.update_interval = 16
+        self.loss = loss_dict[hparams.loss_type]()
 
-        self.loss = NeRFLoss(lambda_distortion=self.hparams.distortion_loss_w)
-        self.train_psnr = PeakSignalNoiseRatio(data_range=1)
-        self.val_psnr = PeakSignalNoiseRatio(data_range=1)
-        self.val_ssim = StructuralSimilarityIndexMeasure(data_range=1)
-        if self.hparams.eval_lpips:
-            self.val_lpips = LearnedPerceptualImagePatchSimilarity('vgg')
-            for p in self.val_lpips.net.parameters():
-                p.requires_grad = False
+        self.embedding_xyz = Embedding(3, 10) # 10 is the default number
+        self.embedding_dir = Embedding(3, 4) # 4 is the default number
+        self.embeddings = [self.embedding_xyz, self.embedding_dir]
 
-        rgb_act = 'None' if self.hparams.use_exposure else 'Sigmoid'
-        self.model = NGP(scale=self.hparams.scale, rgb_act=rgb_act)
-        G = self.model.grid_size
-        self.model.register_buffer('density_grid',
-            torch.zeros(self.model.cascades, G**3))
-        self.model.register_buffer('grid_coords',
-            create_meshgrid3d(G, G, G, False, dtype=torch.int32).reshape(-1, 3))
+        self.nerf_coarse = NeRF()
+        self.models = [self.nerf_coarse]
+        if hparams.N_importance > 0:
+            self.nerf_fine = NeRF()
+            self.models += [self.nerf_fine]
 
-    def forward(self, batch, split):
-        if split=='train':
-            poses = self.poses[batch['img_idxs']]
-            directions = self.directions[batch['pix_idxs']]
-        else:
-            poses = batch['pose']
-            directions = self.directions
+    def decode_batch(self, batch):
+        rays = batch['rays'] # (B, 8)
+        rgbs = batch['rgbs'] # (B, 3)
+        return rays, rgbs
 
-        if self.hparams.optimize_ext:
-            dR = axisangle_to_R(self.dR[batch['img_idxs']])
-            poses[..., :3] = dR @ poses[..., :3]
-            poses[..., 3] += self.dT[batch['img_idxs']]
+    def forward(self, rays):
+        """Do batched inference on rays using chunk."""
+        B = rays.shape[0]
+        results = defaultdict(list)
+        for i in range(0, B, self.hparams.chunk):
+            rendered_ray_chunks = \
+                render_rays(self.models,
+                            self.embeddings,
+                            rays[i:i+self.hparams.chunk],
+                            self.hparams.N_samples,
+                            self.hparams.use_disp,
+                            self.hparams.perturb,
+                            self.hparams.noise_std,
+                            self.hparams.N_importance,
+                            self.hparams.chunk, # chunk size is effective in val mode
+                            self.train_dataset.white_back)
 
-        rays_o, rays_d = get_rays(directions, poses)
+            for k, v in rendered_ray_chunks.items():
+                results[k] += [v]
 
-        kwargs = {'test_time': split!='train',
-                  'random_bg': self.hparams.random_bg}
-        if self.hparams.scale > 0.5:
-            kwargs['exp_step_factor'] = 1/256
-        if self.hparams.use_exposure:
-            kwargs['exposure'] = batch['exposure']
+        for k, v in results.items():
+            results[k] = torch.cat(v, 0)
+        return results
 
-        return render(self.model, rays_o, rays_d, **kwargs)
-
-    def setup(self, stage):
+    def prepare_data(self):
         dataset = dataset_dict[self.hparams.dataset_name]
         kwargs = {'root_dir': self.hparams.root_dir,
-                  'downsample': self.hparams.downsample}
-        self.train_dataset = dataset(split=self.hparams.split, **kwargs)
-        self.train_dataset.batch_size = self.hparams.batch_size
-        self.train_dataset.ray_sampling_strategy = self.hparams.ray_sampling_strategy
-
-        self.test_dataset = dataset(split='test', **kwargs)
+                  'img_wh': tuple(self.hparams.img_wh)}
+        if self.hparams.dataset_name == 'llff':
+            kwargs['spheric_poses'] = self.hparams.spheric_poses
+            kwargs['val_num'] = self.hparams.num_gpus
+        self.train_dataset = dataset(split='train', **kwargs)
+        self.val_dataset = dataset(split='val', **kwargs)
 
     def configure_optimizers(self):
-        # define additional parameters
-        self.register_buffer('directions', self.train_dataset.directions.to(self.device))
-        self.register_buffer('poses', self.train_dataset.poses.to(self.device))
-
-        if self.hparams.optimize_ext:
-            N = len(self.train_dataset.poses)
-            self.register_parameter('dR',
-                nn.Parameter(torch.zeros(N, 3, device=self.device)))
-            self.register_parameter('dT',
-                nn.Parameter(torch.zeros(N, 3, device=self.device)))
-
-        load_ckpt(self.model, self.hparams.weight_path)
-
-        net_params = []
-        for n, p in self.named_parameters():
-            if n not in ['dR', 'dT']: net_params += [p]
-
-        opts = []
-        self.net_opt = FusedAdam(net_params, self.hparams.lr, eps=1e-15)
-        opts += [self.net_opt]
-        if self.hparams.optimize_ext:
-            opts += [FusedAdam([self.dR, self.dT], 1e-6)] # learning rate is hard-coded
-        net_sch = CosineAnnealingLR(self.net_opt,
-                                    self.hparams.num_epochs,
-                                    self.hparams.lr/30)
-
-        return opts, [net_sch]
+        self.optimizer = get_optimizer(self.hparams, self.models)
+        scheduler = get_scheduler(self.hparams, self.optimizer)
+        
+        return [self.optimizer], [scheduler]
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
-                          num_workers=16,
-                          persistent_workers=True,
-                          batch_size=None,
+                          shuffle=True,
+                          num_workers=4,
+                          batch_size=self.hparams.batch_size,
                           pin_memory=True)
 
     def val_dataloader(self):
-        return DataLoader(self.test_dataset,
-                          num_workers=8,
-                          batch_size=None,
+        return DataLoader(self.val_dataset,
+                          shuffle=False,
+                          num_workers=4,
+                          batch_size=1, # validate one image (H*W rays) at a time
                           pin_memory=True)
-
-    def on_train_start(self):
-        self.model.mark_invisible_cells(self.train_dataset.K.to(self.device),
-                                        self.poses,
-                                        self.train_dataset.img_wh)
-
-    def training_step(self, batch, batch_nb, *args):
-        if self.global_step%self.update_interval == 0:
-            self.model.update_density_grid(0.01*MAX_SAMPLES/3**0.5,
-                                           warmup=self.global_step<self.warmup_steps,
-                                           erode=self.hparams.dataset_name=='colmap')
-
-        results = self(batch, split='train')
-        loss_d = self.loss(results, batch)
-        if self.hparams.use_exposure:
-            zero_radiance = torch.zeros(1, 3, device=self.device)
-            unit_exposure_rgb = self.model.log_radiance_to_rgb(zero_radiance,
-                                    **{'exposure': torch.ones(1, 1, device=self.device)})
-            loss_d['unit_exposure'] = \
-                0.5*(unit_exposure_rgb-self.train_dataset.unit_exposure_rgb)**2
-        loss = sum(lo.mean() for lo in loss_d.values())
+    
+    def training_step(self, batch, batch_nb):
+        log = {'lr': get_learning_rate(self.optimizer)}
+        rays, rgbs = self.decode_batch(batch)
+        results = self(rays)
+        log['train/loss'] = loss = self.loss(results, rgbs)
+        typ = 'fine' if 'rgb_fine' in results else 'coarse'
 
         with torch.no_grad():
-            self.train_psnr(results['rgb'], batch['rgb'])
-        self.log('lr', self.net_opt.param_groups[0]['lr'])
-        self.log('train/loss', loss)
-        # ray marching samples per ray (occupied space on the ray)
-        self.log('train/rm_s', results['rm_samples']/len(batch['rgb']), True)
-        # volume rendering samples per ray (stops marching when transmittance drops below 1e-4)
-        self.log('train/vr_s', results['vr_samples']/len(batch['rgb']), True)
-        self.log('train/psnr', self.train_psnr, True)
+            psnr_ = psnr(results[f'rgb_{typ}'], rgbs)
+            log['train/psnr'] = psnr_
 
-        return loss
-
-    def on_validation_start(self):
-        torch.cuda.empty_cache()
-        if not self.hparams.no_save_test:
-            self.val_dir = f'results/{self.hparams.dataset_name}/{self.hparams.exp_name}'
-            os.makedirs(self.val_dir, exist_ok=True)
+        return {'loss': loss,
+                'progress_bar': {'train_psnr': psnr_},
+                'log': log
+               }
 
     def validation_step(self, batch, batch_nb):
-        rgb_gt = batch['rgb']
-        results = self(batch, split='test')
+        rays, rgbs = self.decode_batch(batch)
+        rays = rays.squeeze() # (H*W, 3)
+        rgbs = rgbs.squeeze() # (H*W, 3)
+        results = self(rays)
+        log = {'val_loss': self.loss(results, rgbs)}
+        typ = 'fine' if 'rgb_fine' in results else 'coarse'
+    
+        if batch_nb == 0:
+            W, H = self.hparams.img_wh
+            img = results[f'rgb_{typ}'].view(H, W, 3).cpu()
+            img = img.permute(2, 0, 1) # (3, H, W)
+            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu() # (3, H, W)
+            depth = visualize_depth(results[f'depth_{typ}'].view(H, W)) # (3, H, W)
+            stack = torch.stack([img_gt, img, depth]) # (3, 3, H, W)
+            self.logger.experiment.add_images('val/GT_pred_depth',
+                                               stack, self.global_step)
 
-        logs = {}
-        # compute each metric per image
-        self.val_psnr(results['rgb'], rgb_gt)
-        logs['psnr'] = self.val_psnr.compute()
-        self.val_psnr.reset()
-
-        w, h = self.train_dataset.img_wh
-        rgb_pred = rearrange(results['rgb'], '(h w) c -> 1 c h w', h=h)
-        rgb_gt = rearrange(rgb_gt, '(h w) c -> 1 c h w', h=h)
-        self.val_ssim(rgb_pred, rgb_gt)
-        logs['ssim'] = self.val_ssim.compute()
-        self.val_ssim.reset()
-        if self.hparams.eval_lpips:
-            self.val_lpips(torch.clip(rgb_pred*2-1, -1, 1),
-                           torch.clip(rgb_gt*2-1, -1, 1))
-            logs['lpips'] = self.val_lpips.compute()
-            self.val_lpips.reset()
-
-        if not self.hparams.no_save_test: # save test image to disk
-            idx = batch['img_idxs']
-            rgb_pred = rearrange(results['rgb'].cpu().numpy(), '(h w) c -> h w c', h=h)
-            rgb_pred = (rgb_pred*255).astype(np.uint8)
-            depth = depth2img(rearrange(results['depth'].cpu().numpy(), '(h w) -> h w', h=h))
-            imageio.imsave(os.path.join(self.val_dir, f'{idx:03d}.png'), rgb_pred)
-            imageio.imsave(os.path.join(self.val_dir, f'{idx:03d}_d.png'), depth)
-
-        return logs
+        log['val_psnr'] = psnr(results[f'rgb_{typ}'], rgbs)
+        return log
 
     def validation_epoch_end(self, outputs):
-        psnrs = torch.stack([x['psnr'] for x in outputs])
-        mean_psnr = all_gather_ddp_if_available(psnrs).mean()
-        self.log('test/psnr', mean_psnr, True)
+        mean_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        mean_psnr = torch.stack([x['val_psnr'] for x in outputs]).mean()
 
-        ssims = torch.stack([x['ssim'] for x in outputs])
-        mean_ssim = all_gather_ddp_if_available(ssims).mean()
-        self.log('test/ssim', mean_ssim)
-
-        if self.hparams.eval_lpips:
-            lpipss = torch.stack([x['lpips'] for x in outputs])
-            mean_lpips = all_gather_ddp_if_available(lpipss).mean()
-            self.log('test/lpips_vgg', mean_lpips)
-
-    def get_progress_bar_dict(self):
-        # don't show the version number
-        items = super().get_progress_bar_dict()
-        items.pop("v_num", None)
-        return items
+        return {'progress_bar': {'val_loss': mean_loss,
+                                 'val_psnr': mean_psnr},
+                'log': {'val/loss': mean_loss,
+                        'val/psnr': mean_psnr}
+               }
+    def lr_scheduler_step(self, scheduler, optimizer_idx, monitor_val=None):
+        scheduler.step()
 
 
 if __name__ == '__main__':
     hparams = get_opts()
-    if hparams.val_only and (not hparams.ckpt_path):
-        raise ValueError('You need to provide a @ckpt_path for validation!')
     system = NeRFSystem(hparams)
+    
+    checkpoint_callback = ModelCheckpoint(dirpath=os.path.join(f'ckpts/{hparams.exp_name}',
+                                                                '{epoch:d}'),
+                                          monitor='val/loss',
+                                          mode='min',
+                                          save_top_k=5,)
 
-    ckpt_cb = ModelCheckpoint(dirpath=f'ckpts/{hparams.dataset_name}/{hparams.exp_name}',
-                              filename='{epoch:d}',
-                              save_weights_only=True,
-                              every_n_epochs=hparams.num_epochs,
-                              save_on_train_epoch_end=True,
-                              save_top_k=-1)
-    callbacks = [ckpt_cb, TQDMProgressBar(refresh_rate=1)]
+    logger = TestTubeLogger(
+        save_dir="logs",
+        name=hparams.exp_name,
+        debug=False,
+        create_git_tag=False
+    )
 
-    logger = TensorBoardLogger(save_dir=f"logs/{hparams.dataset_name}",
-                               name=hparams.exp_name,
-                               default_hp_metric=False)
+    early_stop_callback = EarlyStopping(
+        monitor=None,
+        min_delta=0.0,
+        patience=3,
+        verbose=True,
+        mode='min'
+    )
 
-    trainer = Trainer(max_epochs=hparams.num_epochs,
-                      check_val_every_n_epoch=hparams.num_epochs,
-                      callbacks=callbacks,
-                      logger=logger,
-                      enable_model_summary=False,
-                      accelerator='gpu',
-                      devices=hparams.num_gpus,
-                      strategy=DDPPlugin(find_unused_parameters=False)
-                               if hparams.num_gpus>1 else None,
-                      num_sanity_val_steps=-1 if hparams.val_only else 0,
-                      precision=16)
 
-    trainer.fit(system, ckpt_path=hparams.ckpt_path)
+    trainer = Trainer(max_epochs=hparams.num_epochs, 
+              callbacks=[early_stop_callback],
+              checkpoint_callback=checkpoint_callback,
+              resume_from_checkpoint=hparams.ckpt_path,
+              logger=logger,
+              weights_summary=None,
+              progress_bar_refresh_rate=1,
+              gpus=hparams.num_gpus,
+              # distributed_backend='ddp' if hparams.num_gpus>1 else None,
+              strategy='ddp' if hparams.num_gpus>1 else None,
+              num_sanity_val_steps=1,
+              benchmark=True,
+              profiler=PyTorchProfiler()
+              # profiler=hparams.num_gpus==1
+              )
 
-    if not hparams.val_only: # save slimmed ckpt for the last epoch
-        ckpt_ = \
-            slim_ckpt(f'ckpts/{hparams.dataset_name}/{hparams.exp_name}/epoch={hparams.num_epochs-1}.ckpt',
-                      save_poses=hparams.optimize_ext)
-        torch.save(ckpt_, f'ckpts/{hparams.dataset_name}/{hparams.exp_name}/epoch={hparams.num_epochs-1}_slim.ckpt')
-
-    if (not hparams.no_save_test) and \
-       hparams.dataset_name=='nsvf' and \
-       'Synthetic' in hparams.root_dir: # save video
-        imgs = sorted(glob.glob(os.path.join(system.val_dir, '*.png')))
-        imageio.mimsave(os.path.join(system.val_dir, 'rgb.mp4'),
-                        [imageio.imread(img) for img in imgs[::2]],
-                        fps=30, macro_block_size=1)
-        imageio.mimsave(os.path.join(system.val_dir, 'depth.mp4'),
-                        [imageio.imread(img) for img in imgs[1::2]],
-                        fps=30, macro_block_size=1)
+    trainer.fit(system)
